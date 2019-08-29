6592594e19b5bcd7bdbac8cfa4cb72bae1fd67d3
xsu <xsu@thoughtworks.com>
2019-08-29 15:59:53 +0800

    can join

--- src/main/java/tws/mapper/EmployeeMapper.java
+++ src/main/java/tws/mapper/EmployeeMapper.java
@@ -9,14 +9,14 @@ import java.util.List;
 @Mapper
 public interface EmployeeMapper {
 
-    //todo 把company弄进Employee里
     @Results(value = {
             @Result(property = "id", column = "employee_id"),
             @Result(property = "name", column = "name"),
             @Result(property = "age", column = "age"),
-            @Result(property = "company", column = "company_id", one = @One(select = "tws.mapper.CompanyMapper.get"))
+            @Result(property = "company.id", column = "ccc"),
+            @Result(property = "company.name", column = "ddd")
     })
-    @Select("select * from employee e")
+    @Select("select e.employee_id, e.name, e.age, c.id as ccc, c.name as ddd from employee e left join company c on e.company_id = c.id")
     List<Employee> get(int startIndex, int pageSize);
 
     @Insert("insert into employee values (#{employee.id}, #{employee.name}, #{employee.age}, #{companyId})")
