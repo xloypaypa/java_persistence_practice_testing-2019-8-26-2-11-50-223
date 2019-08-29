d45683bcef5a28528d829a9757598f4c42c2489f
xsu <xsu@thoughtworks.com>
2019-08-29 17:22:12 +0800

    add more example

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -17,3 +17,6 @@ insert into company values (1, 'c1');
 insert into company values (2, 'c2');
 
 insert into employee values (1, 'e1', 18, 2);
+insert into employee values (2, 'e2', 18, 1);
+insert into employee values (3, 'e3', 18, 1);
+insert into employee values (4, 'e4', 18, 1);
